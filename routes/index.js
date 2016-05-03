a055ff594346573a548e1620370044be44a3ef23
Austin Green <austingreenkansas@gmail.com>
2016-05-03 09:44:35 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MyFields API' });
+  res.render('index', { title: 'MyFields Mobile API' });
 });
 
 module.exports = router;
