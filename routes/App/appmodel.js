a2b5b5343e9793f4c009c67ac2475b567004c1b8
Jonathan Gooden <jmgooden94@gmail.com>
2016-11-11 11:15:36 -0600

    started routes

--- routes/App/appmodel.js
+++ routes/App/appmodel.js
@@ -4,6 +4,15 @@ var router = express.Router({mergeParams:true});
 var isAuthenticated = require('../Authentication/authenticationMiddlewear').isAuthenticated;
 var isTypes = require('../Authentication/authenticationMiddlewear').isTypes;
 
+/**
+ * @api {get} api/app/ List
+ * @apiName List All App
+ * @apiGroup App
+ *
+ * @apiHeader {String} x-access-token Valid authentication JWT.
+ *
+ * @apiSuccess {array} object App
+ */
 router.get('/', function(req, res, next) {
     models.AppModel.findAll()
         .then(function (result) {
@@ -12,6 +21,34 @@ router.get('/', function(req, res, next) {
         .error(function (err) {
             res.status(500).send(err);
         });
+});
+
+/**
+ * @api {post} api/app/add
+ * @apiName Add App
+ * @apiGroup App
+ * 
+ * @apiHeader {String} x-access-token Valid authentication JWT.
+ * @apiParam {String} name The name of the application
+ * @apiParam {String} devInfo The developer informtion for the app, including version
+ */
+router.post('/app/add', isAuthenticated, isTypes(['Admin']), function(req, res, next){
+    if(!req.body.name || !req.body.devInfo)
+    {
+        res.status(500).send({message: "Missing post parameters."});
+    }
+    else {
+        models.App.create({
+            name: req.body.name,
+            devInfo: req.body.devInfo
+        })
+            .then(function(result) {
+                res.send(result);
+            })
+            .error(function(err){
+                res.status(500).send(err);
+            })
+    }
 });
 
 module.exports = router;
--- routes/AppData/appdata.js
+++ routes/AppData/appdata.js
@@ -4,6 +4,15 @@ var router = express.Router({mergeParams:true});
 var isAuthenticated = require('../Authentication/authenticationMiddlewear').isAuthenticated;
 var isTypes = require('../Authentication/authenticationMiddlewear').isTypes;
 
+/**
+ * @api {get} api/appdata/ List
+ * @apiName List All AppData
+ * @apiGroup AppData
+ *
+ * @apiHeader {String} x-access-token Valid authentication JWT.
+ *
+ * @apiSuccess {array} object App Appdata
+ */
 router.get('/', function(req, res, next) {
     models.AppData.findAll()
         .then(function (result) {
