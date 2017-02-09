3aeb13adcace6f2fbb5fd4f2c959ef1c522aede3
Jonathan Gooden <jmgooden94@gmail.com>
2017-02-09 10:53:16 -0600

    working on oauth - not done

--- routes/Authentication/authentication.js
+++ routes/Authentication/authentication.js
@@ -2,43 +2,52 @@ const express = require('express');
 var jwt    = require('jsonwebtoken');
 var models  = require('../../models');
 var config = require('../../config/config');
+var request = require('request');
+var qs = require('querystring');
 var router = express.Router({mergeParams:true});
 
 'use strict';
 
-const simpleOauthModule = require('simple-oauth2');
+// oauth_consumer_key: process.env.OAUTH_KEY,
+// secret: process.env.OAUTH_SECRET
 
 const app = express();
-const oauth2 = simpleOauthModule.create({
-  client: {
-    //id: process.env.OAUTH_KEY,
-    //secret: process.env.OAUTH_SECRET,
-    id: 'gE73dGnozdQp8Xgj3PEf4dgxez8pa7EN',
-    secret: 'j8Cagf3oG2v9ssX27QkKj4beb9A5UMLS'
-    // Github Testing
-    //id: 'c3ef8c23e04510bc59b3',
-    //secret: '851d07cb1724cd94224e3f5ff4fc19a5594ace18'
-  },
-  auth: {
-    tokenHost: 'https://svcs.ext.solotandem.com:32768',
-    //tokenHost: 'https://github.com',
-    tokenPath: '/oauth/access_token',
-    //tokenPath: '/login/oauth/access_token',
-    authorizePath: '/oauth/authorize'
-    //authorizePath: '/login/oauth/authorize'
-  },
-});
 
-// Authorization uri definition
-const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  // redirect_uri: 'oob'
-  redirect_uri: 'http://10.131.231.41/callback'
+var host = 'https://svcs.ext.solotandem.com:32768';
+var token_path = '/oauth/access_token';
+var authorizePath= '/oauth/authorize';
+
+/*
+app.get('/login', (req, res) => {
+
+  // Step 1: Build a request url to send to Oauth site to request token
+
+  // Step 2: Submit request, and get results
+
+  // Step 3: Redirect user to redirect url supplied with the Oauth results
+
 });
+*/
 
 // Initial page redirecting to myFields
 app.get('/auth', (req, res) => {
-  console.log(authorizationUri);
-  res.redirect(authorizationUri);
+  var qs = require('querystring')
+  var oauth = {
+    oauth_callback: 'oob',
+    oauth_consumer_key: 'gE73dGnozdQp8Xgj3PEf4dgxez8pa7EN',//process.env.OAUTH_KEY,
+    oauth_consumer_secret: 'j8Cagf3oG2v9ssX27QkKj4beb9A5UMLS', //process.env.OAUTH_SECRET
+    oauth_signature_method: 'HMAC-SHA1'
+  }
+
+  // TODO: This is insecure - we need to get a valid certificiate
+  request.post({url:host+token_path, oauth:oauth, rejectUnauthorized: false}, function(err, response, body)
+  {
+    console.log(err)
+    console.log(body)
+    var req_data = qs.parse(body)
+    console.log(req_data)
+    res.end()
+  })
 });
 
 // Callback service parsing the authorization token and asking for the access token
@@ -46,21 +55,7 @@ app.get('/callback', (req, res) => {
   const code = req.query.code;
   const options = {
     code,
-  };
-
-  oauth2.authorizationCode.getToken(options, (error, result) => {
-    if (error) {
-      console.error('Access Token Error', error.message);
-      return res.json('Authentication failed');
-    }
-
-    console.log('The resulting token: ', result);
-    const token = oauth2.accessToken.create(result);
-
-    return res
-      .status(200)
-      .json(token);
-  });
+  }
 });
 
 app.get('/success', (req, res) => {
@@ -74,9 +69,6 @@ app.get('/', (req, res) => {
 app.listen(80, () => {
   console.log('Express server started on port 80');
 });
-
-
-// Credits for the above to [@lazybean](https://github.com/lazybean)
 
 /*
 const express = require('express');
