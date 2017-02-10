58a45cd1ac415c87cc2c3b7128d9ae99df5d6d5f
Jonathan Gooden <jmgooden94@WIN2.CS.KSU.EDU>
2017-02-10 15:04:12 -0600

    working on authentication - think oauth workflow is done

--- routes/Authentication/authentication.js
+++ routes/Authentication/authentication.js
@@ -5,42 +5,25 @@ var config = require('../../config/config');
 var request = require('request');
 var qs = require('querystring');
 var router = express.Router({mergeParams:true});
+const app = express();
 
 'use strict';
 
-// oauth_consumer_key: process.env.OAUTH_KEY,
-// secret: process.env.OAUTH_SECRET
 
-const app = express();
+var oauth_consumer_key: process.env.OAUTH_KEY,
+var oauth_consumer_secret:  process.env.OAUTH_SECRET
 
 var host = 'https://svcs.ext.solotandem.com:32768';
 var token_path = '/oauth/access_token';
 var authorizePath= '/oauth/authorize';
 
-/*
-app.get('/login', (req, res) => {
-
-  // Step 1: Build a request url to send to Oauth site to request token
-
-  // Step 2: Submit request, and get results
-
-  // Step 3: Redirect user to redirect url supplied with the Oauth results
-
-});
-*/
-
 // Initial page redirecting to myFields
 app.get('/auth', (req, res) => {
-  var qs = require('querystring')
-  
-  var oauth_consumer_key: process.env.OAUTH_KEY,
-  var oauth_consumer_secret:  process.env.OAUTH_SECRET
-  
+
   var oauth = {
     oauth_callback: 'oob',
     oauth_consumer_key: oauth_consumer_key,
     oauth_consumer_secret:  oauth_consumer_secret
-    oauth_signature_method: 'HMAC-SHA1'
   }
 
   // TODO: This is insecure - we need to get a valid certificiate
@@ -48,40 +31,35 @@ app.get('/auth', (req, res) => {
   {
     console.log(err)
     console.log(body)
-	// TODO: for some reason this body is empty
+	  // TODO: for some reason this body is empty
     var req_data = qs.parse(body)
     console.log(req_data)
-	// Redirect user to authorize uri
+	  // Redirect user to authorize uri
     var uri = host + authorizePath + '?' + qs.stringify({oauth_token: req_data.oauth_token})
-	// After token is authorized
-	var auth_data = qs.parse(body),
-		oauth = 
-		{
-			consumer_key: consumer_key,
-			consumer_secret: oauth_consumer_secret,
-			token: auth_data.oauth_token,
-			token_secret: req_data.oauth_token_secret,
-			verifier: auth_data.oauth_verifier
-		},
-		url = host + token_path;
-		// TODO: left off here
+	   // After token is authorized
+	  var auth_data = qs.parse(body),
+        oauth =
+        {
+        	consumer_key: oauth_consumer_key,
+        	consumer_secret: oauth_consumer_secret,
+        	token: auth_data.oauth_token,
+        	token_secret: req_data.oauth_token_secret,
+        	verifier: auth_data.oauth_verifier
+        },
+        url = host + token_path;
+    // TODO: I think this is for testing from the tutorial - we probably
+    // just need to store the token and secret - ask Nathan
+    // Tutorial used is here: https://www.npmjs.com/package/request#oauth-signing 
 		request.post({url:url, oauth:oauth}, function (e, r, body) {
-			// ready to make signed requests on behalf of the user 
-			var perm_data = qs.parse(body)
-			  , oauth =
-				{ consumer_key: CONSUMER_KEY
-				, consumer_secret: CONSUMER_SECRET
-				, token: perm_data.oauth_token
-				, token_secret: perm_data.oauth_token_secret
-				}
-			  , url = 'https://api.twitter.com/1.1/users/show.json'
-			  , qs =
-				{ screen_name: perm_data.screen_name
-				, user_id: perm_data.user_id
-				}
-			  ;
-			request.get({url:url, oauth:oauth, qs:qs, json:true}, function (e, r, user) {
-			  console.log(user)
+			// ready to make signed requests on behalf of the user
+			var perm_data = qs.parse(body),
+          oauth =
+  				{
+            consumer_key: CONSUMER_KEY,
+            consumer_secret: CONSUMER_SECRET,
+            token: perm_data.oauth_token,
+            token_secret: perm_data.oauth_token_secret
+  				}
 			})
 	  })
   })
