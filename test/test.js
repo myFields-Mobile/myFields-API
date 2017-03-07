c00376cfc39a76213f641cc4657f2145a8bf5737
McKenzie <mrhine@ksu.edu>
2017-03-07 10:00:04 -0600

    Fixing merge conflicts

--- test/test.js
+++ test/test.js
@@ -277,7 +277,6 @@ describe('Inspection', function() {
         });
     });
 
-<<<<<<< HEAD
     describe('List All Inspections', function (done) {
         it('should list all inspections including the deactive ones', function (done) {
             inspectionTest.listDeactiveInspections(done);
@@ -285,145 +284,113 @@ describe('Inspection', function() {
     });
 });
 
+// 2016-2017 Team's Tests start here
+
 /**
-* App Tests
-*/
-describe('App', function() {
+ * Admin Tests
+ */
+
+describe('Admin', function () {
     before(function (done) {
         done();
-=======
-  // 2016-2017 Team's Tests start here
-
-  /**
-   * Admin Tests
-   */
-
-  describe('Admin', function() {
-    before(function(done) {
-      done();
-    });
-  });
-
-   /**
-   * App Tests
-   */
-
-   describe('App', function() {
-    before(function(done) {
-      done();
-    });
-  });
-
-    describe('List All Apps', function(done){
-      it('should list all of the apps', function(done){
-        appTest.listAllApps(done);
-      });
->>>>>>> dev-master
-    });
-
-    describe('List All Apps', function (done) {
-        it('should list all of the apps', function (done) {
-            appTest.listAllApps(done);
-        });
-    });
-
-    describe('Create Invalid App', function (done) {
-        it('should fail to create an app', function (done) {
-            appTest.createInvalidApp(done);
-        });
-    });
-
-<<<<<<< HEAD
-    describe('Create Valid App', function (done) {
-        it('should succeed to create an app', function (done) {
-            appTest.createValidApp(done);
-        });
-=======
-   /**
-   * AppData Tests
-   */
-
-    describe('AppData', function() {
-        before(function(done) {
-            done();
-        });
-    });
-
-    describe('List All AppData', function(done){
-      it('should list all of the appdata information', function(done){
-        appDataTest.listAllAppData(done);
-      });
->>>>>>> dev-master
     });
 });
 
 /**
-* AppData Tests
-*/
-describe('AppData', function() {
-   before(function (done) {
-       done();
-   });
+ * App Tests
+ */
 
-   describe('List All AppData', function (done) {
-       it('should list all of the appdata information', function (done) {
-           appDataTest.listAllAppData(done);
-       });
-   });
+describe('App', function () {
+    before(function (done) {
+        done();
+    });
+});
 
-   describe('List All AppData JSON Information', function (done) {
-       it('should list all of JSONs within AppData', function (done) {
-           appDataTest.listAllAppDataJSON(done);
-       });
-   });
+describe('List All Apps', function (done) {
+    it('should list all of the apps', function (done) {
+        appTest.listAllApps(done);
+    });
+});
 
-   describe('Create Invalid AppData', function (done) {
-       it('should fail to create an AppData entry', function (done) {
-           appDataTest.createInvalidAppData(done);
-       });
-   });
+describe('Create Invalid App', function (done) {
+    it('should fail to create an app', function (done) {
+        appTest.createInvalidApp(done);
+    });
+});
 
-   describe('Create Valid AppData', function (done) {
-       it('should successfully create an AppData entry', function (done) {
-           appDataTest.createValidAppData(done);
-       });
-   });
+describe('Create Valid App', function (done) {
+    it('should succeed to create an app', function (done) {
+        appTest.createValidApp(done);
+    });
+});
 
-   describe('Remove AppData Fail', function (done) {
-       it('should fail to remove an AppData entry', function (done) {
-           appDataTest.removeAppDataFail(done);
-       });
-   });
+/**
+ * AppData Tests
+ */
+describe('AppData', function () {
+    before(function (done) {
+        done();
+    });
 
-   describe('Remove AppData Success', function (done) {
-       it('should successfully remove an AppData entry', function (done) {
-           appDataTest.removeAppDataSuccess(done);
-       });
-   });
+    describe('List All AppData', function (done) {
+        it('should list all of the appdata information', function (done) {
+            appDataTest.listAllAppData(done);
+        });
+    });
 
-   describe('Filter AppData by User', function (done) {
-       it('should filter the app data by a specific user', function (done) {
-           appDataTest.filterAppDataByUser(done);
-       });
-   });
+    describe('List All AppData JSON Information', function (done) {
+        it('should list all of JSONs within AppData', function (done) {
+            appDataTest.listAllAppDataJSON(done);
+        });
+    });
 
-   describe('Filter AppData by Location', function (done) {
-       it('should filter the app data by a specific location', function (done) {
-           appDataTest.filterAppDataByLocation(done);
-       });
-   });
+    describe('Create Invalid AppData', function (done) {
+        it('should fail to create an AppData entry', function (done) {
+            appDataTest.createInvalidAppData(done);
+        });
+    });
 
-   describe('Filter AppData by App', function (done) {
-       it('should filter the app data by a specific app', function (done) {
-           appDataTest.filterAppDataByApp(done);
-       });
-   });
+    describe('Create Valid AppData', function (done) {
+        it('should successfully create an AppData entry', function (done) {
+            appDataTest.createValidAppData(done);
+        });
+    });
 
-   describe('Get All Images', function (done) {
-       it('should get all images within AppData', function (done) {
-           appDataTest.getAllImages(done);
-       });
-   });
+    describe('Remove AppData Fail', function (done) {
+        it('should fail to remove an AppData entry', function (done) {
+            appDataTest.removeAppDataFail(done);
+        });
+    });
+
+    describe('Remove AppData Success', function (done) {
+        it('should successfully remove an AppData entry', function (done) {
+            appDataTest.removeAppDataSuccess(done);
+        });
+    });
+
+    describe('Filter AppData by User', function (done) {
+        it('should filter the app data by a specific user', function (done) {
+            appDataTest.filterAppDataByUser(done);
+        });
+    });
+
+    describe('Filter AppData by Location', function (done) {
+        it('should filter the app data by a specific location', function (done) {
+            appDataTest.filterAppDataByLocation(done);
+        });
+    });
+
+    describe('Filter AppData by App', function (done) {
+        it('should filter the app data by a specific app', function (done) {
+            appDataTest.filterAppDataByApp(done);
+        });
+    });
+
+    describe('Get All Images', function (done) {
+        it('should get all images within AppData', function (done) {
+            appDataTest.getAllImages(done);
+        });
+    });
 });
 
 /**
@@ -434,64 +401,45 @@ describe('Images', function () {
         done();
     });
 
-    describe('List all blob info', function(done)
-    {
-        it('should list all blob information', function (done)
-        {
+    describe('List all blob info', function (done) {
+        it('should list all blob information', function (done) {
             imagesTest.listBlobs(done);
         });
     });
 
-    describe('Invalid blob addition', function(done)
-    {
-       it('should fail to add a blob', function(done)
-       {
-          imagesTest.addBlobFail(done);
-       });
+    describe('Invalid blob addition', function (done) {
+        it('should fail to add a blob', function (done) {
+            imagesTest.addBlobFail(done);
+        });
     });
 
-    describe('Valid blob addition', function(done)
-    {
-       it('should succeed to upload a blob', function (done)
-       {
-          imagesTest.addBlobSuccess(done);
-       });
+    describe('Valid blob addition', function (done) {
+        it('should succeed to upload a blob', function (done) {
+            imagesTest.addBlobSuccess(done);
+        });
     });
 
-    describe('Invalid blob stream reading', function (done)
-    {
-       it('should fail to read a blob stream', function (done)
-       {
-          imagesTest.readBlobFail(done);
-       });
+    describe('Invalid blob stream reading', function (done) {
+        it('should fail to read a blob stream', function (done) {
+            imagesTest.readBlobFail(done);
+        });
     });
 
-    describe('Valid blob stream reading', function (done)
-    {
-        it('should succeed to read a blob stream', function (done)
-        {
+    describe('Valid blob stream reading', function (done) {
+        it('should succeed to read a blob stream', function (done) {
             imagesTest.readBlobSuccess(done);
         });
     });
 
-    describe('Invalid blob deletion', function (done)
-    {
-       it('should fail in deleting a blob', function (done)
-       {
-          imagesTest.deleteBlobFail(done);
-       });
+    describe('Invalid blob deletion', function (done) {
+        it('should fail in deleting a blob', function (done) {
+            imagesTest.deleteBlobFail(done);
+        });
     });
 
-    describe('Valid blob deletion', function (done)
-    {
-        it('should succeed in deleting a blob', function (done)
-        {
+    describe('Valid blob deletion', function (done) {
+        it('should succeed in deleting a blob', function (done) {
             imagesTest.deleteBlobSuccess(done);
         });
     });
-<<<<<<< HEAD
 });
-=======
-
-});
->>>>>>> dev-master
