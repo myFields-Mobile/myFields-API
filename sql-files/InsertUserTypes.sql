9253184f23df2ac6bc3762a1dfc0bd8cb5691bf0
Jonathan Gooden <jmgooden94@gmail.com>
2017-04-13 10:39:39 -0500

    deleting last team's stuff

--- sql-files/InsertUserTypes.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Admin', NOW(), NOW());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Researcher', NOW(), NOW());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Farmer', NOW(), NOW());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('User', NOW(), NOW());
-
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Admin', DATETIME(), DATETIME());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Researcher', DATETIME(), DATETIME());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('Farmer', DATETIME(), DATETIME());
-INSERT INTO UserTypes (title, createdAt, updatedAt) VALUES ('User', DATETIME(), DATETIME());
