1dff7807941a9d104c3ff5de21745bdaefaf0a7c
Jonathan Gooden <jmgooden94@gmail.com>
2017-04-27 10:40:34 -0500

    working on sessions

--- app.js
+++ app.js
@@ -16,17 +16,14 @@ app.set('view engine', 'jade');
 
 // session set up
 var sessions = require("client-sessions");
-console.log('secret', process.env.COOKIE_SECRET);
+console.log('secret', process.env.SESSION_COOKIE_SECRET);
 app.use(sessions({
   cookieName: 'oauth_cookie', // cookie name dictates the key name added to the request object
-  secret: process.env.COOKIE_SECRET, // should be a large unguessable string
+  secret: process.env.SESSION_COOKIE_SECRET, // should be a large unguessable string
   duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
   activeDuration: 1000 * 60 * 15 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 }));
 
-
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(function(req, res, next) {
