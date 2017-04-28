8381f57c7f1e89f29c2e1781383b9d29376d2ed5
Jonathan Gooden <jmgooden94@gmail.com>
2017-04-28 15:24:20 -0500

    working on sessions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var session = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var api = require('./routes/api');
 
@@ -15,12 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // session set up
-var sessions = require("client-sessions");
-app.use(sessions({
-  cookieName: 'oauth_cookie', // cookie name dictates the key name added to the request object
-  secret: process.env.COOKIE_SECRET, // should be a large unguessable string
-  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 1000 * 60 * 15 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+app.use(session({
+  //cookieName: 'oauth_cookie', // cookie name dictates the key name added to the request object
+  secret: process.env.COOKIE_SECRET // should be a large unguessable string
 }));
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
