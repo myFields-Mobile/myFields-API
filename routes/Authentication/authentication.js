b7886d064cf735e9877dd13b6757b384cb89584e
Jonathan Gooden <jmgooden94@gmail.com>
2017-05-04 14:03:06 -0500

    working on cleaning up oauth

--- routes/Authentication/authentication.js
+++ routes/Authentication/authentication.js
@@ -59,11 +59,10 @@ router.get('/', (req, res) => {
 *
 * @apiSuccess {object} user_oauth signed in user's oauth credentials
 */
-router.get('/callback', (req, res, x, y) => {
+router.get('/callback', (req, res, x) => {
   console.log("63", req)
   console.log("64", res)
   console.log("65", x)
-  console.log("66", y)
   var auth_data = qs.parse(req.query)
   var oauth =
     { 
